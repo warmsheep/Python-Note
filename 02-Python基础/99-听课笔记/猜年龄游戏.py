c70bee6c9278daac1ae9baa320c8caefda7690b5
Lin Xuan <warmsheep@me.com>
2018-01-10 23:31:12 +0800

    新增第6小节内容

--- 02-Python基础/99-听课笔记/猜年龄游戏.py
+++ 02-Python基础/99-听课笔记/猜年龄游戏.py
@@ -2,16 +2,17 @@
 # 用户最多可以猜三次
 
 
-count = 0
-age = 22
-while count%3 > 0:
+while count < 3:
     user_guess = int( input("your guess:") )
     if user_guess == age :
         print("恭喜你，答对了！")
-    elif user_guess > age :
+        break
+    elif user_guess > age:
         print("try smaller")
     else :
         print("try bigger")
-while count%3 = 0:
-    choice = 
-    count += 1
+    count +=1
+    while count == 3:
+        choice = input('你还想要继续玩么？如果是请输入Y或者y')
+        if choice == "y" or "Y":
+            count = 0
