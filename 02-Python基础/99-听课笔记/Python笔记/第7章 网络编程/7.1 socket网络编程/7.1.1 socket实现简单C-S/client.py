6a06839b14a5d66a5ad746fdfa34fe31ff2db7c2
袁俊 <2859626066@qq.com>
2018-05-04 16:36:50 +0800

    更新

--- 02-Python基础/99-听课笔记/Python笔记/第7章 网络编程/7.1 socket网络编程/7.1.1 socket实现简单C-S/client.py
+++
@@ -1,6 +0,0 @@
-import socket
-client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-hostname=socket.gethostname()
-client.connect((hostname,8080))
-client.send("hello".encode("utf-8"))
-client.close()
