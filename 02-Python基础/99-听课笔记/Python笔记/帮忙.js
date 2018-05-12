05af1942db98a7b565d16ff410e367d8c925afac
袁俊 <2859626066@qq.com>
2018-05-12 15:21:43 +0800

    更新

--- 02-Python基础/99-听课笔记/Python笔记/帮忙.js
+++ 02-Python基础/99-听课笔记/Python笔记/帮忙.js
@@ -1,11 +1,4 @@
 ```gantt
-mermaid.ganttConfig = {
-    titleTopMargin:25,
-    barHeight:20,
-    barGap:4,
-    topPadding:75,
-    sidePadding:75
-}
     title 食堂课题研究进度表
     section 研究项目
         指定计划 :a1, 2018-04-05, 1d
@@ -32,7 +25,7 @@ mermaid.ganttConfig = {
         概念验证       : 5d
     section 项目实施
         概要设计      :2016-07-05  , 5d
-        详细设计      :2016-07-08, 10d  
+        详细设计      :2016-07-08, 10d
         编码          :2016-07-15, 10d
         测试          :2016-07-22, 5d
     section 发布验收
